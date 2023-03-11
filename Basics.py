786cef520ec1937bb4545e9c75a364238e88b4a2
SudhaVaagdevi0118 <127116947+SudhaVaagdevi0118@users.noreply.github.com>
2023-03-11 19:27:27 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,56 @@
+print("Hello Wprld")
+demo = 20    #int
+print(demo) 
+demo1 = 120.09  #float
+print(demo1)
+name = "sudha"   #string
+print(name)
+name1 = 'A'     #char
+print(name1)
+print(type(demo))
+print(type(demo1))
+print(type(name))
+demo2 =float(77)
+print(demo2)
+demo3 = int(77.6601)
+print(demo3)
+demo4 = str(77)
+print(demo4)
+print(type(demo4))
+demo5 = True   #boolean data type True/False
+print(type(demo5))
+car = "Range Rover"
+print(type(car))
+print(car)
+print(car[0])
+print(car[5])
+print(car[6])
+print(len(car))
+demo6 = "I want to learn Python"
+print("learn" in demo6)
+print("sudha" in demo6)
+demo7 = "My first python program "
+print(demo7[0])
+print(demo7[0:8])
+print(demo7[:5])
+print(demo7[6:])
+print(demo7[-6:])
+demo8 = "hello world"
+print(demo8.upper())
+print(demo8.lower())
+demo9 = "Sudha"
+demo10 = "Rani"
+print(demo9 + demo10)
+print(demo9 +" "+ demo10)
+#boolean
+print(15<20)
+print(10 == 11)
+print(10 == 10)
+# Arithmetic operators
+print(10 + 20)
+print(10 * 10)
+print(10 - 10)
+print(10 / 10)
+print(10 % 2)
+print(2 ** 3) #2 power 3
+print(15 // 2)
